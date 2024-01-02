f15c1176700fe566912e37fea14bc1042468845e
hugues2 <hugues.flament@gmail.com>
2024-01-02 13:30:16 +0100

    First version of school

---
+++ addons/school/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name':'School',
+    'version':'1.1',
+    'summary':" School management system",
+    'sequence':1,
+    'description': "this is a school management system",
+    'category':'',
+    'website':'http://www.lalibre.be',
+    'depends':['base']
+}
