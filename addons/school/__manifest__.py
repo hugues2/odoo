5f15b78b94280bfa4b4a1fddf4a2f4315c549261
hugues2 <hugues.flament@gmail.com>
2024-01-02 13:41:46 +0100

    First version of school

--- addons/school/__manifest__.py
+++ addons/school/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name':'School',
     'version':'1.1',
-    'author':'Lajuar OÜ'
+    'author':'Lajuar OÜ',
     'summary':" School management system",
     'sequence':1,
     'description': "this is a school management system",
