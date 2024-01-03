6e4686a9a87989999e26d873a88a86f4f8ea7637
hugues2 <hugues.flament@gmail.com>
2024-01-03 15:48:35 +0100

    First version of school

--- addons/school/models/school.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class SchoolProfile(models.Model):
-    _name="school.profile"
-
-    name=fields.Char(string="School Name")
-    email=fields.Char(string="Email")
-    phone=fields.Char("Phone")
-
---
+++ addons/student/models/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class Student(models.Model):
+    _name = "wb.student"
+    _description = "this is student profile"
+    name=fields.Char("Name")
+    name1=fields.Char("Name1")
+    name2=fields.Char("Name2")
+    name3=fields.Char("Name3")
