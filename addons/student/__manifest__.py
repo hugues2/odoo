564891635d19ec3bec6af020f5b5fd4975e84723
hugues2 <hugues.flament@gmail.com>
2024-01-03 16:18:41 +0100

    First version of school

--- addons/student/__manifest__.py
+++ addons/student/__manifest__.py
@@ -22,7 +22,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/student_view.xml'
     ]
 }
