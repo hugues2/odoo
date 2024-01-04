cae8b70ebe90d1fb1855641d5700eae790155696
hugues2 <hugues.flament@gmail.com>
2024-01-04 15:47:32 +0100

    First version of school

--- addons/student/models/models.py
+++ addons/student/models/models.py
@@ -27,7 +27,7 @@ class Student(models.Model):
                 "client_id": "2IaeiZXbcKzlP1KvjZH9ghty2IJKM8Lg",
                 "secret": "56RkLgZREDi1H0HZAvzOSAVlxu1Flx41",
                 "container_type": "pdf",
-                "signature_redirect": "google.com",
+                "signature_redirect": "https://google.com",
                 "notification_state":
                     {
                     "time": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S%z')
