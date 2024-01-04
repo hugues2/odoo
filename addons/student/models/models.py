3ce656ca7b0be47a7cf71b01f1b9dfbef3113d2e
hugues2 <hugues.flament@gmail.com>
2024-01-04 10:04:47 +0100

    First version of school

--- addons/student/models/models.py
+++ addons/student/models/models.py
@@ -9,6 +9,7 @@ class Student(models.Model):
     name=fields.Char("Name")
     name1=fields.Char("Name1")
     name2=fields.Char("Name2")
+    file_to_sign = fields.Binary(string=’File to sign’ )
 
     def action_do_something(self):
         for record in self:
