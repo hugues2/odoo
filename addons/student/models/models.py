4a18db65f0470671ce53b32f71d79514acd62a2c
hugues2 <hugues.flament@gmail.com>
2024-01-04 13:50:50 +0100

    First version of school

--- addons/student/models/models.py
+++ addons/student/models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import base64
+import base64, datetime, requests
 
 from odoo import models, fields, api
 
@@ -11,11 +11,44 @@ class Student(models.Model):
     name1=fields.Char("Name1")
     name2=fields.Char("Name2")
     file_to_sign = fields.Binary(string="File to sign")
+    filename=fields.Char()
 
     def action_do_something(self):
         for record in self:
             record.name = "Something"
-            print (base64.b64encode(record.file_to_sign))
-            print("hello")
+            eIDEasy_request ={
+                "files": [
+                    {
+                        "fileContent": base64.b64encode(record.file_to_sign).decode('utf-8'),
+                        "fileName": record.filename,
+                        "mimeType": "application/pdf"
+                    }
+                    ],
+                "client_id": "2IaeiZXbcKzlP1KvjZH9ghty2IJKM8Lg",
+                "secret": "56RkLgZREDi1H0HZAvzOSAVlxu1Flx41",
+                "container_type": "pdf",
+                "signature_redirect": "google.com",
+                "notification_state":
+                    {
+                    "time": datetime.datetime.now()
+                    }
+                }
+            api_url = "https://test.eideasy.com/api/signatures/prepare-files-for-signing"
+
+            # Set the Content-Type header to indicate JSON data
+            headers = {"Content-Type": "application/json"}
+
+            # Make the GET request
+            response = requests.get(api_url,json=eIDEasy_request, headers=headers)
+
+
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Process the API response (assuming it's in JSON format)
+                api_data = response.json()
+                # Now you can use `api_data` in your Odoo model logic
+            else:
+                # Handle the error appropriately
+                print(f"API request failed with status code: {response.status_code}")
 
         return True
