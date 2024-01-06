fb8c7e3b42d403b77fd1b0ac4e6bc8b732a3b56a
hugues2 <hugues.flament@gmail.com>
2024-01-06 09:26:31 +0100

    First version of school

--- addons/student/controllers/main_controller.py
+++ addons/student/controllers/main_controller.py
@@ -8,5 +8,5 @@ class MainController(http.Controller):
     def main_method(self, **kw):
         param_value = request.params.get('param_name')
         print("Return from eID Easy")
-        return True
-        #return http.request.render('your_module.template_name', {'param_value': param_value})
+        #return True
+        return http.request.render('your_module.template_name', {'param_value': param_value})
