89694985ddb2f2d67103b4865916a79197378424
hugues2 <hugues.flament@gmail.com>
2024-01-06 09:51:57 +0100

    First version of school

--- addons/student/controllers/__init__.py
+++ addons/student/controllers/__init__.py
@@ -1,1 +1,1 @@
-from . import main_controller
+from . import controllers
---
+++ addons/student/controllers/controllers.py
@@ -0,0 +1,10 @@
+# controllers/controller.py
+
+
+# -*- coding: utf-8 -*-
+from odoo import http
+class Test(http.Controller):
+    @http.route('/afterSigning', auth='public')
+    def index(self, **kw):
+        print("Return from eID Easy")
+        return "Hello, world"
