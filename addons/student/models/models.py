66056ec8121feafdddfdfc1f6fe5e9fdee6a7e91
hugues2 <hugues.flament@gmail.com>
2024-01-07 17:47:11 +0100

    First version of school

--- addons/student/models/models.py
+++ addons/student/models/models.py
@@ -41,7 +41,7 @@ class Student(models.Model):
                 "client_id": "2IaeiZXbcKzlP1KvjZH9ghty2IJKM8Lg",
                 "secret": "56RkLgZREDi1H0HZAvzOSAVlxu1Flx41",
                 "container_type": "pdf",
-                "signature_redirect": request.httprequest.host_url+'afterSigning'&'returnUrl='+http.request.httprequest.full_path,
+                "signature_redirect": request.httprequest.host_url+'afterSigning&returnUrl='+http.request.httprequest.full_path,
                 "notification_state":
                     {
                     "time": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S%z')
